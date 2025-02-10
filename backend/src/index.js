10b6d9983b70871534b224ba09ad28abb538a445
Shrey Ghodke <ghodkeshrey@gmail.com>
2025-02-11 01:45:12 +0530

    Updated frontend and backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://events-application-mu.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
